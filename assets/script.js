10259f6dd2667f8da062b10d7e94f88cf8da1ced
Guilherme <contatoguigvd@hotmail.com>
2022-11-01 20:50:33 -0300

    click botao não

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,10 @@ document.getElementById('btn-sim').addEventListener('click', function(){
     alert('Agora somos namorados <3')
 })
 
+document.getElementById('btn-nao').addEventListener('click', function(){
+    alert('Achou que tinha conseguido??')
+})
+
 document.getElementById('btn-nao').addEventListener('mouseover', function(){
     let posT = Math.floor(Math.random() * (300 - 10) + 10);
     let posR = Math.floor(Math.random() * (300 - 10) + 10);
