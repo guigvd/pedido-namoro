2d8c21b10259d796817e5573b1952334f1d9b4ad
Guilherme <contatoguigvd@hotmail.com>
2022-11-01 17:54:51 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-sim').addEventListener('click', function(){
+    alert('Agora somos namorados <3')
+})
+
+document.getElementById('btn-nao').addEventListener('mouseover', function(){
+    let posT = Math.floor(Math.random() * (300 - 10) + 10);
+    let posR = Math.floor(Math.random() * (300 - 10) + 10);
+
+    document.getElementById('btn-nao').style.top = posT + "px";
+    document.getElementById('btn-nao').style.right = posR + "px";
+})
+
