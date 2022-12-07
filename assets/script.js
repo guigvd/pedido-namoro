2485472c9c0405934a835ea50c70dc0cd505b4a2
Guilherme Veloso <100156111+guigvd@users.noreply.github.com>
2022-12-07 00:55:04 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,28 @@
-document.getElementById('btn-sim').addEventListener('click', function(){
+document.getElementById('btn-sim').addEventListener('click', function () {
     alert('Agora somos namorados <3')
 })
 
-document.getElementById('btn-nao').addEventListener('click', function(){
+document.getElementById('btn-nao').addEventListener('click', function () {
     alert('Achou que tinha conseguido??')
-})
 
-document.getElementById('btn-nao').addEventListener('mouseover', function(){
-    let posT = Math.floor(Math.random() * (300 - 10) + 10);
-    let posR = Math.floor(Math.random() * (300 - 10) + 10);
+    let width = window.screen.width /4;
+    var height = window.screen.height /4;
+
+    let posT = Math.floor(Math.random() * (height - (-height) +1) + (-height));
+    let posR = Math.floor(Math.random() * (width - (-width) +1) + (-width));
 
     document.getElementById('btn-nao').style.top = posT + "px";
     document.getElementById('btn-nao').style.right = posR + "px";
 })
 
+document.getElementById('btn-nao').addEventListener('mouseover', function () {
+    
+    let width = window.screen.width /4;
+    var height = window.screen.height /4;
+
+    let posT = Math.floor(Math.random() * (height - (-height) +1) + (-height));
+    let posR = Math.floor(Math.random() * (width - (-width) +1) + (-width));
+
+    document.getElementById('btn-nao').style.top = posT + "px";
+    document.getElementById('btn-nao').style.right = posR + "px";
+})
